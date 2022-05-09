facdf1221b765f3022f588e60495284576258196
bm43 <hyungjip777@gmail.com>
2022-05-09 23:30:44 +0900

    added limit order functions

--- limit_ob.hpp
+++ limit_ob.hpp
@@ -25,6 +25,53 @@ class LimitOrderBook {
             // implemented in std::unordered_map
             ordermap.clear();
         }
+
+        inline void limit_buy(UID order_id, Quantity quantity, Price price) {
+            ordermap.emplace(std::piecewise_construct,
+                // piecewise_construct is passed as 1st arg to construct a pair obj
+                // https://stackoverflow.com/questions/6162201/c11-use-case-for-piecewise-construct-of-pair-and-tuple
+                std::forward_as_tuple(order_id),
+                std::forward_as_tuple(order_id, Side::Buy, quantity, price)
+            );
+            if (selltree.best != nullptr && price >= selltree.best->key) {
+                selltree.market(&ordermap.at(order_id), [&](UID uid) {
+                    ordermap.erase(uid);
+                });
+                if (ordermap.at(order_id).quantity == 0) {
+                    ordermap.erase(order_id);
+                    return;
+                }
+            }
+            buytree.placeLimit(&ordermap.at(order_id));
+        }
+
+        inline void limit_sell(UID order_id, Quantity quantity, Price price) {
+            ordermap.emplace(std::piecewise_construct,
+                // piecewise_construct is passed as 1st arg to construct a pair obj
+                // https://stackoverflow.com/questions/6162201/c11-use-case-for-piecewise-construct-of-pair-and-tuple
+                std::forward_as_tuple(order_id),
+                std::forward_as_tuple(order_id, Side::Sell, quantity, price)
+            );
+            if (buytree.best != nullptr && price <= buytree.best->key) {
+                buytree.market(&ordermap.at(order_id), [&](UID uid) {
+                    ordermap.erase(uid);
+                });
+                if (ordermap.at(order_id).quantity == 0) {
+                    ordermap.erase(order_id);
+                    return;
+                }
+            }
+            selltree.placeLimit(&ordermap.at(order_id));
+        }
+
+        inline bool has(UID order_id) const {
+            return ordermap.count(order_id);
+        }
+
+        inline const Order& get(UID order_id) {
+            return ordermap.at(order_id);
+        }
+
 };
 
 }
