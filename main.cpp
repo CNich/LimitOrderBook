cf77f0f5e24718b4e49140c0144edf5a8b245381
Hyung Jip LEE <40831181+bm43@users.noreply.github.com>
2022-05-11 13:15:22 +0900

    testing buy orders

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 using namespace LOB;
 
 int main() {
+    /*
     LimitTree<Side::Buy> buytree;
     UID id = 23;
     Quantity qt = 110;
@@ -15,5 +16,21 @@ int main() {
     Order order{id, Side::Sell, qt, 0};
     Order* opt = &order;
     buytree.placeLimit(opt);
+    */
+
+    // aim: 4k executions per sec
+    // execution = order?
+    std::default_random_engine gen;
+    auto price = std::normal_distribution<double>(500, 20);
+    auto quantity = std::normal_distribution<double>(1000,200);
+    LimitOrderBook limitob;
+    for (int i = 0; i < 4000; i++) {
+        auto p = static_cast<Price>(price(gen));
+        auto q = static_cast<Quantity>(quantity(gen));
+        limitob.limit(Side::Buy, 0, 100, 500);
+        // what to print?
+    }
+    
+
     
 }
